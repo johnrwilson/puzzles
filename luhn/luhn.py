0dcadf36d185c25f81a98cc4da65170df1b5562b
John Wilson <johnrwilson@uchicago.edu>
2019-09-18 15:08:30 +0000

    Initial edit for luhn project

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,6 +1,10 @@
 class Luhn(object):
     def __init__(self, card_num):
-        pass
+        if len(card_num) < 2:
+            raise valueError ("Invalid string - input must be at least 2 characters long.")
+        else:
+            self.card_num = card_num
 
     def valid(self):
-        pass
+        try:
+            card_num = int(self.card_num)
