8fa62ef5bb7c8cbea317b22e52fac568fda77f09
Laurence O'Brien <laurence.obrien@chicagobooth.edu>
2019-09-18 15:29:33 +0000

    Darts

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,10 @@
 def score(x, y):
-    pass
+    import math
+    distance = math.sqrt(x**2 + y**2)
+    if distance <= 1:
+        return 10
+    elif distance > 1 and distance <= 5:
+        return 5
+    elif distance > 5 and distance <= 10:
+        return 1
+    return 0
