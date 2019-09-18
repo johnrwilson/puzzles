d3ae3f65d60e635d3fd9c4b17b1ca492b7fcac5c
John Wilson <johnrwilson@uchicago.edu>
2019-09-18 16:23:26 +0000

    Began affine cipher

--- affine-cipher/affine_cipher.py
+++ affine-cipher/affine_cipher.py
@@ -1,6 +1,17 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+          'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+          'u', 'v', 'w', 'x', 'y', 'z']
+lttr2num = {}
+for i in range(len(letters)):
+    lttr2num[letters[i]] = i
+        
+print(lttr2num)
+
 def encode(plain_text, a, b):
     pass
 
 
 def decode(ciphered_text, a, b):
     pass
+
+
